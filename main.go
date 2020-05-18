99b1e8b624835a671dd2fc8dbb32b59e3a6b7ef5
ahmedselim2017 <ahmedselimuzum@gmail.com>
2020-05-18 13:00:04 +0300

    added limit and some small fixes

--- main.go
+++ main.go
@@ -43,27 +43,29 @@ type json_image_source struct {
 
 func main() {
 	var subreddit string
+	var mode string
 	var minH int
 	var minW int
-	var mode string
+	var limit int
 	var filter_nsfw bool
 
-	flag.StringVar(&subreddit, "subreddit", "wallpapers", "Set subreddit name")
-	flag.StringVar(&mode, "mode", "random", "Set download mode (random, hot, new, rising)")
+	flag.StringVar(&subreddit, "subreddit", "wallpapers", "Subreddit name")
+	flag.StringVar(&mode, "mode", "hot", "Download mode (hot, new, rising)")
 	flag.BoolVar(&filter_nsfw, "filter_nsfw", true, "Filter NSFW posts")
-	flag.IntVar(&minH, "minh", 0, "Set minimum height")
-	flag.IntVar(&minW, "minw", 0, "Set minimum width")
+	flag.IntVar(&minH, "minh", 0, "Minimum height")
+	flag.IntVar(&minW, "minw", 0, "Minimum width")
+	flag.IntVar(&limit, "limit", 25, "The maximum length of the posts list which is used for randomly selecting a post (maxiumum 100)")
 	flag.Parse()
 
 	switch mode {
-	case "random", "hot", "new", "rising":
+	case "hot", "new", "rising":
 	default:
 		fmt.Fprintf(os.Stderr, "%s mode not exists\nUsage:\n", mode)
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	imageUrl, err := get_url(subreddit, "random", filter_nsfw)
+	imageUrl, err := get_url(subreddit, mode, minH, minW, limit, filter_nsfw)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err.Error())
 		os.Exit(1)
@@ -71,15 +73,12 @@ func main() {
 	fmt.Printf(imageUrl)
 }
 
-func get_url(subreddit string, mode string, filter_nsfw bool) (string, error) {
-	real_mode := mode
-	if mode == "random" {
-		real_mode = "hot"
-	}
-	url := fmt.Sprintf("https://reddit.com/r/%s/%s.json", subreddit, real_mode)
+func get_url(subreddit string, mode string, minH int, minW int, limit int, filter_nsfw bool) (string, error) {
+	url := fmt.Sprintf("https://reddit.com/r/%s/%s.json", subreddit, mode)
 
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set("User-agent", "wallpaper downloader")
+    req.Header.Set("limit", string(limit))
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not get from r/%s. Error:%s\n", subreddit, err.Error())
@@ -90,18 +89,15 @@ func get_url(subreddit string, mode string, filter_nsfw bool) (string, error) {
 	var jo jsonResponse
 	json.NewDecoder(resp.Body).Decode(&jo)
 
-	childs := jo.Data.Children
-	if mode == "random" {
-		childs = Shuffle(jo.Data.Children)
-	}
+	childs := Shuffle(jo.Data.Children)
 
 	for _, child := range childs {
 		if strings.HasSuffix(child.Data.Url, ".png") || strings.HasSuffix(child.Data.Url, ".jpg") {
 			width := child.Data.Preview.Images[0].Source.Width
 			height := child.Data.Preview.Images[0].Source.Height
 
-			// Ignore filtered and portrait images
-			if (filter_nsfw && child.Data.Over18) || width < height {
+			// Ignore filtered, portrait images and check for minW and minH
+			if (filter_nsfw && child.Data.Over18) || width < minW || height < minH || float32(width) < float32(height) * 1.5 {
 				continue
 			} else {
 				return child.Data.Url, nil
